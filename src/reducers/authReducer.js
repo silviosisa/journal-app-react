fc5556948a62fff6d29cfb770ddad66658a92c23
Silvio Sisa <silviosisa@MacBook-Pro-de-Silvio.local>
2020-12-30 19:09:20 -0300

    created reducer and types files

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,26 @@
+
+/*
+{
+    uid: jslkdfjlsakdjflkasdf,
+    name: 'Silvio'
+}
+ */
+
+
+import {types} from "../types/types";
+
+export const authReducer = (state = {}, action) => {
+    switch (action.type) {
+        case types.login:
+            return {
+                uid: action.payload.uid,
+                name: action.payload.displayName
+            }
+        case types.login:
+            return {
+            }
+        default:
+            return state;
+
+    }
+}
