634101d01483a910ed9c750f4a13bb4aae3381a1
Silvio Sisa <silviosisa@MacBook-Pro-de-Silvio.local>
2020-12-30 18:57:30 -0300

    note screen details

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -21,7 +21,7 @@ export const NoteScreen = () => {
                 <div className="notes__image">
                     <img
                         src="https://images.unsplash.com/photo-1446569971295-057569541991?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1381&q=80"
-                        alt="image beach"/>
+                        alt="beach"/>
                 </div>
             </div>
         </div>
