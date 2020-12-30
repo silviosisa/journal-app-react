e4f72625e01143a666560df614842e071e5e7072
Silvio Sisa <silviosisa@MacBook-Pro-de-Silvio.local>
2020-12-30 14:55:37 -0300

    component when nothing is selected

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
-export const JournalEntri = () => {
+export const JournalEntry = () => {
     return (
-        <div>
-
+        <div className="journal__entry pointer">
+            <div
+                className="journal__entry-picture"
+                style={{
+                    backgroundSize: 'cover',
+                    backgroundImage: 'url(https://images.unsplash.com/photo-1500106029603-6402f3cc585c?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80)'
+                }}
+            >
+            </div>
+            <div className="journal__entry-body">
+                <p className="journal_entry-title">
+                    Un nuevo día
+                </p>
+                <p className="journal_entry-content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+            </div>
+            <div className="journal__entry-date-box">
+                <span>Monday</span>
+                <h4>28</h4>
+            </div>
         </div>
     );
 };
--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,9 +1,15 @@
 import * as React from 'react';
+import {Sidebar} from "./Sidebar";
+// import {NothingSelected} from "./NothingSelected";
 
 export const JournalScreen = () => {
     return (
         <div className="journal__main-content">
-            <h1>JournalScreen</h1>
+            <Sidebar />
+
+            <main>
+                {/*<NothingSelected/>*/}
+            </main>
         </div>
     );
 };
