c3b90cf2813348ad0d431e3b433f5d895258a316
Silvio Sisa <silviosisa@MacBook-Pro-de-Silvio.local>
2021-01-04 19:15:30 -0300

    238 Formulario de registro de usuarios

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,24 +1,46 @@
 import * as React from 'react';
 import {Link} from "react-router-dom";
+import {useForm} from "../../hooks/useForm";
 
 export const RegisterScreen = () => {
+
+    const [ formValues, handleInputChanges , reset ] = useForm({
+        name: 'sil',
+        email: 'silviosisa@gmail.com',
+        password: '123456',
+        password2: '123456'
+    });
+
+    const { name, email, password, password2 } = formValues;
+
+
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        console.log(name,email, password, password2);
+    }
+
     return (
         <>
             <h3 className="auth__title">Register</h3>
-            <form autoComplete="off">
+            <form onSubmit={handleRegister} autoComplete="off">
                 <input
                     type="text"
                     placeholder="Name"
                     name="name"
                     className="auth__input"
                     autoComplete="off"
+                    value={ name }
+                    onChange={ handleInputChanges }
                 />
                 <input
                     type="text"
                     placeholder="Email"
-                    name="emailtest"
+                    name="email"
                     className="auth__input"
                     autoComplete="off"
+                    value={ email }
+                    onChange={ handleInputChanges }
                 />
                 <input
                     type="password"
@@ -26,6 +48,8 @@ export const RegisterScreen = () => {
                     name="password"
                     className="auth__input"
                     autoComplete="new-password"
+                    value={ password }
+                    onChange={ handleInputChanges }
                 />
                 <input
                     type="password"
@@ -33,6 +57,8 @@ export const RegisterScreen = () => {
                     name="password2"
                     className="auth__input"
                     autoComplete="new-password"
+                    value={ password2 }
+                    onChange={ handleInputChanges }
                 />
                 <button
                     className="btn btn-primary btn-block mb-5"
