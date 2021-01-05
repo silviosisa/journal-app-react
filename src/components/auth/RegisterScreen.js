06f3699fe3a97a64f8e744dc64113fbc4ee87427
Silvio Sisa <silviosisa@MacBook-Pro-de-Silvio.local>
2021-01-04 22:56:30 -0300

    239 manejo de errores del formulario y uiReducer y Acciones

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import {Link} from "react-router-dom";
 import {useForm} from "../../hooks/useForm";
+import validator from 'validator';
+import {useDispatch} from "react-redux";
+import {removeError, setError} from "../../actions/ui";
 
 export const RegisterScreen = () => {
+
+    const dispatch =  useDispatch();
 
     const [ formValues, handleInputChanges , reset ] = useForm({
         name: 'sil',
@@ -17,13 +22,36 @@ export const RegisterScreen = () => {
 
     const handleRegister = (e) => {
         e.preventDefault();
-        console.log(name,email, password, password2);
+       if( ifFormValid()){
+        console.log('Formulario correcto');
+       }
+    }
+
+    const ifFormValid = () => {
+        if(name.trim().length === 0){
+            console.log('name is required');
+            dispatch(setError('name is required'));
+            return false;
+        } else if ( !validator.isEmail(email)){
+            console.log('Email is not valid');
+            dispatch(setError('Email is not valid'));
+            return false;
+        } else if ( password !== password2 || password.length < 5){
+            console.log('Password should be at least 6 characters and mactch each other');
+            dispatch(setError('Password should be at least 6 characters and mactch each other'));
+            return false
+        }
+        dispatch(removeError());
+        return true;
     }
 
     return (
         <>
             <h3 className="auth__title">Register</h3>
             <form onSubmit={handleRegister} autoComplete="off">
+                <div className="auth__alert-error">
+                    Hola Mundo
+                </div>
                 <input
                     type="text"
                     placeholder="Name"
