3229190db07d293b09f358c860ccd29ece1cfb5f
Silvio Sisa <silviosisa@gmail.com>
2021-01-11 12:54:49 -0300

    cambio en el store

--- src/store/store.js
+++ src/store/store.js
@@ -6,7 +6,6 @@ import { uiReducer } from "../reducers/uiReducer";
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const reducers = combineReducers({
-    auth: authReducer,
     ui: uiReducer
 });
 
