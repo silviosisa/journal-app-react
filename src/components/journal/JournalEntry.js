bbcfe91e03d0d9b77f17de52559c88d407437b93
Silvio Sisa <silviosisa@gmail.com>
2021-01-15 18:14:09 -0300

    260 moment.js Mostrar las notas en el menu

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,27 +1,34 @@
 import React from 'react';
+import moment from 'moment';
 
-export const JournalEntry = () => {
+export const JournalEntry = ({id, date, title, body, url}) => {
+
+    const noteDate = moment(date);
+
     return (
         <div className="journal__entry pointer">
-            <div
-                className="journal__entry-picture"
-                style={{
-                    backgroundSize: 'cover',
-                    backgroundImage: 'url(https://images.unsplash.com/photo-1500106029603-6402f3cc585c?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80)'
-                }}
-            >
-            </div>
+            {
+                url &&
+                <div
+                    className="journal__entry-picture"
+                    style={{
+                        backgroundSize: 'cover',
+                        backgroundImage: `url(${url})`
+                    }}
+                >
+                </div>
+            }
             <div className="journal__entry-body">
                 <p className="journal_entry-title">
-                    Un nuevo día
+                    {title}
                 </p>
                 <p className="journal_entry-content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    {body}
                 </p>
             </div>
             <div className="journal__entry-date-box">
-                <span>Monday</span>
-                <h4>28</h4>
+                <span>{noteDate.format('dddd')}</span>
+                <h4>{noteDate.format('Do')}</h4>
             </div>
         </div>
     );
