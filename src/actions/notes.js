c6cb50bb9ee9a290812d8b1a6ae717f2bcb337ec
Silvio Sisa <silviosisa@gmail.com>
2021-01-15 17:55:47 -0300

    optimizacion de peticion

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,5 +1,6 @@
 import {db} from "../firebase/firebase-config";
 import {types} from "../types/types";
+import {loadNotes} from "../helpers/loadNotes";
 
 export const startNewNote = () => {
     return async(dispatch, getState) =>{
@@ -27,3 +28,15 @@ export const activeNote = (id, note) => (
         }
     }
 )
+
+export const startLoadingNotes = (uid) => {
+    return async( dispatch ) => {
+        const notes = await loadNotes( uid);
+        dispatch(setNotes(notes));
+    }
+}
+
+export const setNotes = (notes) => ({
+    type: types.notesLoad,
+    payload: notes
+})
