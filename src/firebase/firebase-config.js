479c522d974c8523460c91b8e7d1d76d15aaec86
Silvio Sisa <silviosisa@gmail.com>
2021-02-01 15:13:31 -0300

    complete firebase

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,12 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB6sF49u6PMyAS2Ch4v2NZHbGAZRybYkO8",
-    authDomain: "react-app-cursos-70d4b.firebaseapp.com",
-    projectId: "react-app-cursos-70d4b",
-    storageBucket: "react-app-cursos-70d4b.appspot.com",
-    messagingSenderId: "269522642506",
-    appId: "1:269522642506:web:a50df8ee36aed60958a8b3"
+
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
